a57544862fe497a1a327a5abae868313115c7a7d
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-02 06:05:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,6 +15,5 @@ def display_board(board = [" "," "," "," "," "," "," "," "," "])
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   11.times {print "-"}
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
-
-  
