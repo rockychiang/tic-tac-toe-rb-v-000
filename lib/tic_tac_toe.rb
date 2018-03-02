f9a89f4c51e252c0ad9b640cb3cfb02c14357aed
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-02 06:40:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,8 +46,10 @@ end
 
 def turn_count(board)
   counter = 0
-  board.each do |i| i == "X" || i == "O"
-    counter += 1
+  board.each do |i|
+    if i == "X" || i == "O"
+      counter += 1
+    end
   end
   counter
 end
